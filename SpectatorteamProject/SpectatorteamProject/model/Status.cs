a59342c9562bb08c429b35833312c2616ddf71bb
Tilda Böröcz <borocz.tilda@gmail.com>
2021-09-16 12:29:44 +0200

    name status osztalyok

---
+++ SpectatorteamProject/SpectatorteamProject/model/Status.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectatorteamProject.model
+{
+        
+        /// <summary>
+        /// enum=read-only típusú különleges osztály, Státusz osztály
+        /// </summary>
+        enum Status
+        {
+            /// <summary>
+            /// enum típusnál elemek: állapotai
+            /// </summary>
+            Inactive,
+            Active,
+            Expired
+        }
+        /// <summary>
+        /// név osztály
+        /// </summary>
+        
+    }
+
